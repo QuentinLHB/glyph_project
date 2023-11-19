ee6b257d9bdc9281233d90e790d08ded8395acbc
QuentinLHB <q.lehembre@gmail.com>
2023-11-19 20:34:14 +0100

    code clean

--- lib/controllers/message_controller.dart
+++ lib/controllers/message_controller.dart
@@ -8,131 +8,52 @@ import '../models/conversation.dart';
 import '../models/message.dart';
 
 class MessageController {
+  /// Token used to retrieve messages from URL
   String? _token;
+
   static final MessageController _instance = MessageController._internal();
 
   MessageController._internal();
 
+  /// Initialises the token
   Future<void> initToken() async {
-    if (_token == null) _token = await getToken();
+    _token ??= await getToken();
   }
 
   static MessageController get instance => _instance;
 
+  /// Message written by the user
   final List<ComplexGlyph> _messageBeingWritten = <ComplexGlyph>[];
 
   List<ComplexGlyph> get messageBeingWritten => _messageBeingWritten;
 
+  /// Adds a glyph to the current message
   void addGlyph(ComplexGlyph newGlyph) {
     messageBeingWritten.add(newGlyph);
   }
 
-  void modifyGlyph(ComplexGlyph glyphToModify, ComplexGlyph newGlyph) {
+  /// Replaces a glyph with another in the current message.
+  void replaceGlyph(ComplexGlyph glyphToModify, ComplexGlyph newGlyph) {
     int index = messageBeingWritten
         .indexWhere((item) => identical(item, glyphToModify));
     messageBeingWritten.removeAt(index);
     messageBeingWritten.insert(index, newGlyph);
   }
 
+  /// Deletes a glyph from the current message.
   void deleteGlyph(ComplexGlyph glyphToDelete) {
     messageBeingWritten.removeWhere((item) => identical(item, glyphToDelete));
   }
 
+  /// Deletes the current message.
   void clearMessage() {
     messageBeingWritten.clear();
   }
 
-  Future<bool> updateFileOnGitHub(
-      String filePath, String content, String token) async {
-    try {
-      final response = await http
-          .get(Uri.parse(filePath), headers: {'Authorization': 'token $token'});
+  // region SEND_MESAGE
 
-      if (response.statusCode == 200) {
-        final sha = jsonDecode(response.body)['sha'];
-
-        final updateResponse = await http.put(
-          Uri.parse(filePath),
-          headers: {
-            'Authorization': 'token $token',
-            'Content-Type': 'application/json'
-          },
-          body: jsonEncode({
-            'message': 'update file',
-            'content': base64Encode(utf8.encode(content)),
-            'sha': sha,
-          }),
-        );
-
-        if (updateResponse.statusCode == 200) {
-          print('File updated');
-          return true;
-        } else {
-          print('Failed to update file: ${updateResponse.body}');
-          return false;
-        }
-      } else {
-        print('Failed to get file: ${response.body}');
-        return false;
-      }
-    } catch (e) {
-      print(e);
-      return false;
-    }
-  }
-
-  Message createMessage(String sender, List<ComplexGlyph> complexGlyphs) {
-    // Convertir chaque ComplexGlyph en une liste d'IDs
-    List<List<int>> glyphIdGroups =
-        complexGlyphs.map((complexGlyph) => complexGlyph.ids).toList();
-
-    return Message(
-      sender: sender,
-      timestamp: DateTime.now(),
-      glyphIds: glyphIdGroups,
-    );
-  }
-
-  String createMessageJson(String sender, List<ComplexGlyph> glyphs) {
-    Message message = createMessage(sender, glyphs);
-    String jsonMessage = jsonEncode(message.toJson());
-    return jsonMessage;
-  }
-
-  Future<bool> validateMessage(Conversation conversation, String sender) async {
-    // Message newMessage = createMessageJson(sender, messageBeingWritten);
-    Message newMessage = createMessage(sender, messageBeingWritten);
-    bool success = await _sendMessage(conversation, newMessage);
-    if(success) {
-      conversation.messages.add(newMessage);
-      messageBeingWritten.clear();
-    }
-    return success;
-  }
-
-  // Future<Conversation?> getConversation(Conversation conversation) async {
-  //   final response = await http.get(
-  //     Uri.parse(conversation.url),
-  //     headers: {
-  //       'Authorization': 'token $_token',
-  //       'Accept': 'application/vnd.github.v3.raw',
-  //     },
-  //   );
-  //   if (response.statusCode == 200) {
-  //     // Étape 2: Décoder le contenu JSON
-  //     var data = jsonDecode(response.body);
-  //
-  //     String title = data['title'];
-  //     List<Message> messages = (data['messages'] as List).map((item) =>
-  //         Message.fromJson(item)).toList();
-  //
-  //     return Conversation.loaded(title, conversation.url);
-  //   }else {
-  //     print('Failed to read file: ${response.body}');
-  //     return null;
-  //   }
-  // }
-
+  /// Retrieves messages history of a conversation.
+  /// Returns a Conversation object filled with Message objects.
   Future<Conversation?> loadConversationMessage(
       Conversation conversation) async {
     try {
@@ -172,6 +93,33 @@ class MessageController {
     }
   }
 
+  /// Creates a message
+  Message createMessage(String sender, List<ComplexGlyph> complexGlyphs) {
+    // Convertir chaque ComplexGlyph en une liste d'IDs
+    List<List<int>> glyphIdGroups =
+        complexGlyphs.map((complexGlyph) => complexGlyph.ids).toList();
+
+    return Message(
+      sender: sender,
+      timestamp: DateTime.now(),
+      glyphIds: glyphIdGroups,
+    );
+  }
+
+  /// Validates the [messageBeingWritten] by uploading it on server.
+  /// Returns [true] if the operation succeeded.
+  Future<bool> validateMessage(Conversation conversation, String sender) async {
+    // Message newMessage = createMessageJson(sender, messageBeingWritten);
+    Message newMessage = createMessage(sender, messageBeingWritten);
+    bool success = await _sendMessage(conversation, newMessage);
+    if (success) {
+      conversation.messages.add(newMessage);
+      clearMessage();
+    }
+    return success;
+  }
+
+  /// Sends a [Message] to the distant file.
   Future<bool> _sendMessage(
       Conversation conversation, Message messageToAdd) async {
     try {
@@ -184,18 +132,60 @@ class MessageController {
 
       // Étape 5: Écrire le nouveau JSON dans le fichier
       // Utilisez ici la méthode pour écrire dans votre fichier sur GitHub
-      if (_token != null){
-        bool success = await updateFileOnGitHub(conversation.url, updatedJson, _token!);
+      if (_token != null) {
+        bool success =
+        await updateFileOnGitHub(conversation.url, updatedJson, _token!);
         return success;
-      }else {
+      } else {
         return false;
       }
-
-
     } catch (e) {
       return false;
     }
   }
+
+
+  /// Sends  [content] to a github [url],
+  Future<bool> updateFileOnGitHub(
+      String url, String content, String token) async {
+    try {
+      final response = await http
+          .get(Uri.parse(url), headers: {'Authorization': 'token $token'});
+
+      if (response.statusCode == 200) {
+        final sha = jsonDecode(response.body)['sha'];
+
+        final updateResponse = await http.put(
+          Uri.parse(url),
+          headers: {
+            'Authorization': 'token $token',
+            'Content-Type': 'application/json'
+          },
+          body: jsonEncode({
+            'message': 'update file',
+            'content': base64Encode(utf8.encode(content)),
+            'sha': sha,
+          }),
+        );
+
+        if (updateResponse.statusCode == 200) {
+          print('File updated');
+          return true;
+        } else {
+          print('Failed to update file: ${updateResponse.body}');
+          return false;
+        }
+      } else {
+        print('Failed to get file: ${response.body}');
+        return false;
+      }
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+
 
   Future<String> getToken() async {
     final response = await http.get(
